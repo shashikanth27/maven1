a7d8779999ddbf0770c3731b45f603d0228cafac
shashikanth27 <shashikanth27@gmail.com>
2022-02-27 17:16:18 +0530

    pom file src file commited

---
+++ src/main/java/org/shashi/maven/First_Maven_Project1/App.java
@@ -0,0 +1,13 @@
+package org.shashi.maven.First_Maven_Project1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! " );
+    }
+}
